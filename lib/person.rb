3cd718ad7af0b1a8cd0140d4e7da818066130482
Whittney Davis <wcdavis89@yahoo.com>
2019-11-18 23:55:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
   end
   def name=(full_name)
     first_name, last_name = full_name.split
-    first_name=@first_name
-    last_name=@last_name
+    @first_name=first_name
+    @last_name=last_name
   end
