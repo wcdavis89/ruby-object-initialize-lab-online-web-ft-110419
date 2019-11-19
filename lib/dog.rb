10a4aa105962aaa56a8009db7b7c9feb6da96b23
Whittney Davis <wcdavis89@yahoo.com>
2019-11-19 00:15:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+  def name=(name,breed="Mutt")
+    @name= string
+    @breed = string
+  end
+  def name
+    @name 
+  end
+  def breed
+    @breed
+  end
+end
