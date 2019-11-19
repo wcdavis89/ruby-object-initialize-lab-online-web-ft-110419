fc7fe78f1d4d54a5f5637dcfc8f0d6516704b15a
Whittney Davis <wcdavis89@yahoo.com>
2019-11-19 00:04:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,16 @@ class Dog
   def initialize(name)
     @name=name
   end
-  def initialize(breed)
-     @breed=breed
-  end
+
   def name=(string)
     @name= string
   end
   def name
     @name 
   end
-  
+    def initialize(breed)
+     @breed=breed
+  end
   def breed=(string)
     @breed = string
   end
